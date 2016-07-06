3353fa196d722a0506fee1b51bdb06b843832dfd
Daniel Evans <evansdar@unos.corp>
2016-07-06 14:38:10 -0400

    buttons and main activity

--- app/src/main/java/com/example/evansdar/primer1/MainActivity.java
+++ app/src/main/java/com/example/evansdar/primer1/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.evansdar.primer1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +20,72 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button agenda     = (Button)findViewById(R.id.agendaButton);
+        Button ppts       = (Button)findViewById(R.id.pptsButton);
+        Button attendees  = (Button)findViewById(R.id.attendeesButton);
+        Button speakers   = (Button)findViewById(R.id.speakersButton);
+        Button exhibitors = (Button) findViewById(R.id.exhibitorsButton);
+        // fetchJSON();
+
+    }
+
+    public void onClickToAgenda(View v){
+        System.out.println("agenda");
+
+        // Intent iA = new Intent(this, AgendaEvent.class);
+        // startActivity(iA);
+    }
+
+    public void onClickToAttendees(View v){
+        System.out.println("attendees");
+        //  Intent iA = new Intent(this,Exhibitors.class);
+        // startActivity(iA);
+    }
+
+    public void onClickToSpeakers(View v){
+        // Intent iA = new Intent(this,SpeakerActivity.class);
+        // startActivity(iA);
+        System.out.println("speakers");
+    }
+
+    public void onClickToExhibitors(View v){
+        System.out.println("exhibitors");
+        //  Intent iA = new Intent(this,Exhibitors.class);
+        // startActivity(iA);
+    }
+
+    public void onClickToPPTs(View v){
+        //Intent iA = new Intent(this,PresentationSlidesActivity.class);
+        // startActivity(iA);
+        System.out.println("ppts");
+    }
+
+
+//    public void fetchJSON(){
+//
+//
+//        try {
+//            JSONArray array = new RetrieveJsonArrayTask().execute("https://unosmanticoreapi.azurewebsites.net/Help/Api/GET-api-Locations").get();
+//
+//
+//
+//
+//
+//
+//        }catch(InterruptedException e){
+//            e.printStackTrace();
+//        }catch(ExecutionException f){
+//            f.printStackTrace();
+//        }
+//    }
+
+    public String changeAgendaText(){
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date current = new Date();
+        System.out.print(sdf.format(current));
+        return "";
 
     }
 }
+
